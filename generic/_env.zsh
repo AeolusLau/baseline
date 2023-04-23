e9346dd99e621233a732b3897fc868bea2f829f5
liulichao <liulichao@kuaishou.com>
2023-04-23 13:32:57 +0800

    Add more valuable environment configure

---
+++ generic/_env.zsh
@@ -0,0 +1,25 @@
+# Replace homebrew source
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+#export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
+
+# Enable llvm installed from Homebrew.
+export PATH="/usr/local/opt/llvm/bin:$PATH"
+# Guide cmake to use llvm installed form Homebrew, not XCode.
+export CC=clang
+export CXX=clang++
+
+# Make make faster...
+export MAKEFLAGS='-j8'
+
+export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
+
+# Used to filter out test file in git commands, e.g.,
+#   $ git show HEAD $NT
+export NT=':!**/*test*'
+
+alias vi=nvim
+alias view='nvim -R'
+alias rga="rg --no-config --smart-case --type-add 'cpp:*.ipp' --type c --type cpp --type java --type kotlin --type objc --type objcpp --type swift --type gradle --type cmake"
+alias rgn='rg --no-config --smart-case --no-ignore'
+
+[[ ! -f ~/.env.zsh.local ]] || source ~/.env.zsh.local
